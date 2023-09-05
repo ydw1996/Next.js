bc6c06f0e59b7641c3c9852c35d4a1144016bc5b
Dawit yang <112791174+ydw1996@users.noreply.github.com>
2023-09-06 00:49:48 +0900

    nextjs setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  // 다른 설정들...
+};
