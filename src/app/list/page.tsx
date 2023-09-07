93b6cba48cdf57e7e880b65d0612066d76409032
Dawit yang <112791174+ydw1996@users.noreply.github.com>
2023-09-07 23:52:34 +0900

    tailwind css, mui setting

---
+++ src/app/list/page.tsx
@@ -0,0 +1,13 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return (
+    <Box className="bg-black h-screen w-full p-4">
+      <Button variant="contained"></Button>
+      <Box className="text-black">리스트</Box>
+    </Box>
+  );
+};
+
+export default page;
